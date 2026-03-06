843af3468d787670741aac54676a3f380c4d407e
Pedro Cazelatto <pedrocazelatto@gmail.com>
2026-03-06 16:07:28 -0300

    Adicionar tipo optional

--- src/domain/entities/question.entity.ts
+++ src/domain/entities/question.entity.ts
@@ -1,9 +1,28 @@
 import { Entity } from "../../core/entities/entity"
+import type { UniqueEntityId } from "../../core/entities/unique-entity-id.value-object"
+import type { Optional } from "../../core/types/optional.type"
+import type { Slug } from "./value-objects/slug.value-object"
 
 interface QuestionProps {
+  authorId: UniqueEntityId
+  bestAnswerId?: UniqueEntityId
   title: string
   content: string
-  authorId: string
+  slug: Slug
+  createdAt: Date
+  updatedAt?: Date
 }
 
-export class Question extends Entity<QuestionProps> {}
+export class Question extends Entity<QuestionProps> {
+  static create(
+    props: Optional<QuestionProps, "createdAt">,
+    id?: UniqueEntityId
+  ) {
+    const question = new Question({
+      ...props,
+      createdAt: new Date()
+    }, id)
+
+    return question
+  }
+}
