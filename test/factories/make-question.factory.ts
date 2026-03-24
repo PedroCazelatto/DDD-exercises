b9232d7ad5eeddb2ca17fd664b9834e6994b09ba
Pedro Cazelatto <pedrocazelatto@gmail.com>
2026-03-24 09:12:17 -0300

    Adicionar sentenças fake

--- test/factories/make-question.factory.ts
+++ test/factories/make-question.factory.ts
@@ -3,14 +3,21 @@ import {
   Question,
   type QuestionProps
 } from "@/domain/forum/enterprise/entities/question.entity"
+import { faker } from "@faker-js/faker"
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityId(),
-    title: "Example title",
-    content: "Example content",
-    ...override
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityId(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override
+    },
+    id
+  )
 
   return question
 }
